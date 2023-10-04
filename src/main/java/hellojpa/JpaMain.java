7bd249c8ca808bcaaf90e9566599af4444babee4
k9want <zndwlrnjr26@daum.net>
2023-10-04 21:52:18 +0900

    :tada: init Project

---
+++ src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,42 @@
+package hellojpa;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+/*
+            Member member = new Member();
+            member.setId(2L);
+            member.setName("HelloB");
+
+            em.persist(member);
+
+            Member findMember = em.find(Member.class, 1L);
+
+            em.remove(findMember);*/
+
+            Member findMember = em.find(Member.class, 1L);
+            findMember.setName("HelloJPA");
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+            System.out.println("catch??");
+        } finally {
+            em.close();
+        }
+        emf.close();
+
+    }
+}
